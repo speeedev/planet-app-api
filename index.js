96eb6206fbb40908f9467e7141468752a4aac2b7
Emir Bolat <emirybolat@hotmail.com>
2024-07-15 18:30:11 +0300

    edit index file

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
-const planets = JSON.parse(fs.readFileSync('planets.json', 'utf8'));
+const planets = JSON.parse(fs.readFileSync("data/planets.json", "utf8"));
 
-app.get('/planets', (req, res) => {
-  const { id, name, imageUrl, orderOfProximityToSun, shortDescription, longDescription, primaryColor, videoUrl } = req.query;
+app.get("/", (req, res) => {
+  res.send("Welcome. Please visit /planets");
+});
 
+app.get("/planets", (req, res) => {
+  try {
+    const { fields } = req.query;
 
+    const requestedFields = fields ? fields.split(",") : [];
 
-  const response = {};
-  if (id !== undefined) response.id
-  if (name !== undefined) response.name
-  if (imageUrl !== undefined) response.imageUrl
-  if (orderOfProximityToSun !== undefined)
-  if (shortDescription !== undefined)
-  if (longDescription !== undefined)
-  if (primaryColor !== undefined)
-  if (videoUrl !== undefined)
+    const response = planets.map((planet) => {
+      const planetData = { id: planet.id };
 
-  res.json(response);
+      if (requestedFields.length > 0) {
+        requestedFields.forEach((field) => {
+          if (planet.hasOwnProperty(field)) {
+            planetData[field] = planet[field];
+          }
+        });
+      } else {
+        Object.assign(planetData, planet);
+      }
+
+      return planetData;
+    });
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send({ error: "Server error" });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Server started: http://localhost:${port}`);
+  console.log(`Server runned: http://localhost:${port}`);
 });
