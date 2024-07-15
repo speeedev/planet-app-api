06c319b95b7c003d4e50739e7054702195b6c3a3
Emir Bolat <emirybolat@hotmail.com>
2024-07-15 18:04:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+const planets = JSON.parse(fs.readFileSync('planets.json', 'utf8'));
+
+app.get('/planets', (req, res) => {
+  const { id, name, imageUrl, orderOfProximityToSun, shortDescription, longDescription, primaryColor, videoUrl } = req.query;
+
+
+
+  const response = {};
+  if (id !== undefined) response.id
+  if (name !== undefined) response.name
+  if (imageUrl !== undefined) response.imageUrl
+  if (orderOfProximityToSun !== undefined)
+  if (shortDescription !== undefined)
+  if (longDescription !== undefined)
+  if (primaryColor !== undefined)
+  if (videoUrl !== undefined)
+
+  res.json(response);
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu çalışıyor: http://localhost:${port}`);
+});
